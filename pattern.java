83e4b1a9aa61a3f692c36529e71d77d534cf3b07
jaya <112403482+Jayaaa508@users.noreply.github.com>
2024-07-07 20:45:26 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,12 @@
+//pattern 2
+public class Solution {
+    public static void nForest(int n) {
+       for ( int i =0 ; i<n ;i++){
+           for( int j = 0 ; j<=i;j++ ){
+               System.out.print("* ");
+            }
+           System.out.println();
+       }
+    }
+}
+//pattern 3
