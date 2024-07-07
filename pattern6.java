2ad82fe83f3069ff7c4b91a81435fbc67deb1725
jaya <112403482+Jayaaa508@users.noreply.github.com>
2024-07-07 21:16:43 +0530

    Create pattern6.java

---
+++ pattern6.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public static void nNumberTriangle(int n) {
+        for( int i = 0;i<n ;i++){
+            for(int j = n ; j>i; j--){
+                System.out.print(n-j+1+" ");
+            }
+        System.out.println();    
+        }
+    }
+}
